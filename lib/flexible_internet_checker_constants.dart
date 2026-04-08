166b827c5ff444989acb687aca2d717ff1ddc087
Daler <108223768+oldHarvester@users.noreply.github.com>
2026-04-08 16:47:13 +0500

    fix

--- lib/flexible_internet_checker_constants.dart
+++ lib/flexible_internet_checker_constants.dart
@@ -18,21 +18,17 @@ abstract class FlexibleInternetCheckerConstants {
   static const String URL_1 = 'https://one.one.one.one';
 
   /// URL 2
-  static const String URL_2 = 'https://jsonplaceholder.typicode.com/albums/1';
+  static const String URL_2 = 'https://dns.google';
 
   /// URL 3
-  static const String URL_3 = 'https://fakestoreapi.com/products/1';
-
-  /// URL 4
-  static const String URL_4 = 'https://icanhazip.com/';
+  static const String URL_3 = 'http://captive.apple.com';
 
   /// Default list of addresses to check connectivity against.
   // ignore: non_constant_identifier_names
   static final List<AddressCheckOption> DEFAULT_ADDRESSES =
       List<AddressCheckOption>.unmodifiable(<AddressCheckOption>[
-        AddressCheckOption(uri: Uri.parse(URL_1)),
-        AddressCheckOption(uri: Uri.parse(URL_2)),
-        AddressCheckOption(uri: Uri.parse(URL_3)),
-        AddressCheckOption(uri: Uri.parse(URL_4)),
-      ]);
+    AddressCheckOption(uri: Uri.parse(URL_1)),
+    AddressCheckOption(uri: Uri.parse(URL_2)),
+    AddressCheckOption(uri: Uri.parse(URL_3)),
+  ]);
 }
